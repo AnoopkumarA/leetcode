2980c2f360c29f8eb80a6be281c136b32e54356d
Anoop Kumar A <92391766+AnoopkumarA@users.noreply.github.com>
2024-09-02 11:26:00 +0530

    Update servicecomponents.js

--- src/components/Acertinity/servicecomponents.js
+++ src/components/Acertinity/servicecomponents.js
@@ -2,17 +2,17 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import ProjectCard from "./service";
 const projectData = [
     {
-        imageSrc: "/src/assets/web-design.png",
+        imageSrc: "./src/assets/web-design.png",
         title: "Web Design",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     },
     {
-        imageSrc: "/src/assets/poster.png",
+        imageSrc: "./src/assets/poster.png",
         title: "Poster Design",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     },
     {
-        imageSrc: "/src/assets/web.png",
+        imageSrc: "./src/assets/web.png",
         title: "Web Development",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     },
